86c68100e2515c011938d23f8f0ee85d50944fc1
aegistz <teerayoot5056@gmail.com>
2019-03-05 11:05:51 +0700

    gg

--- register.php
+++ register.php
@@ -78,114 +78,114 @@
 
 	  );"; 
 
-	  require 'scripts/phpmailer/PHPMailerAutoload.php';
+	 //  require 'scripts/phpmailer/PHPMailerAutoload.php';
 
-		header('Content-Type: text/html; charset=utf-8');
+		// header('Content-Type: text/html; charset=utf-8');
 
-		$mail = new PHPMailer;
-		$mail->CharSet = "utf-8";
-		$mail->isSMTP();
-		$mail->Host = 'smtp.gmail.com';
-		$mail->Port = 587;
-		$mail->SMTPSecure = 'tls';
-		$mail->SMTPAuth = true;
-
-
-		$gmail_username = "gistnu@gmail.com"; // gmail ที่ใช้ส่ง
-		$gmail_password = "gistnu2017nu"; // รหัสผ่าน gmail
-		// ตั้งค่าอนุญาตการใช้งานได้ที่นี่ https://myaccount.google.com/lesssecureapps?pli=1
+		// $mail = new PHPMailer;
+		// $mail->CharSet = "utf-8";
+		// $mail->isSMTP();
+		// $mail->Host = 'smtp.gmail.com';
+		// $mail->Port = 587;
+		// $mail->SMTPSecure = 'tls';
+		// $mail->SMTPAuth = true;
 
 
-		$sender = "gistnu"; // ชื่อผู้ส่ง
-		$email_sender = "gistnu@NU.com"; // เมล์ผู้ส่ง 
-		$email_receiver = $email; // เมล์ผู้รับ ***
-
-		$subject = "การยืนยันการสมัค"; // หัวข้อเมล์
+		// $gmail_username = "gistnu@gmail.com"; // gmail ที่ใช้ส่ง
+		// $gmail_password = "gistnu2017nu"; // รหัสผ่าน gmail
+		// // ตั้งค่าอนุญาตการใช้งานได้ที่นี่ https://myaccount.google.com/lesssecureapps?pli=1
 
 
-		$mail->Username = $gmail_username;
-		$mail->Password = $gmail_password;
-		$mail->setFrom($email_sender, $sender);
-		$mail->addAddress($email_receiver);
-		$mail->Subject = $subject;
+		// $sender = "gistnu"; // ชื่อผู้ส่ง
+		// $email_sender = "gistnu@NU.com"; // เมล์ผู้ส่ง 
+		// $email_receiver = $email; // เมล์ผู้รับ ***
 
-		$email_content = "
-			<!DOCTYPE html>
-		  <html>
-		    <head>
-		      <meta charset=utf-8'/>
-		      <title>การกดยืนยันการสมัค</title>
-		    </head>
-		    <body>
-		      <div style='background: #214163;padding: 10px 0 20px 10px;margin-bottom:10px;font-size:30px;color:white;' >
-		        <img src='https://drive.google.com/open?id=1d2dbqiwRVm4W7JfLpdHx17PKVL4aY1PR' style='width: 120px;'>
-		        <div style='text-align:center'> 
-		           <p>กดที่ลิงค์ เพื่อเปลี่ยนพาสเวิร์ด</p><br>
-		           <p>URL : http://localhost:81/jobgis/checkmail.php?email=$email </p>
-		        </div>
-		      </div>
-		        <div>       
+		// $subject = "การยืนยันการสมัค"; // หัวข้อเมล์
+
+
+		// $mail->Username = $gmail_username;
+		// $mail->Password = $gmail_password;
+		// $mail->setFrom($email_sender, $sender);
+		// $mail->addAddress($email_receiver);
+		// $mail->Subject = $subject;
+
+		// $email_content = "
+		// 	<!DOCTYPE html>
+		//   <html>
+		//     <head>
+		//       <meta charset=utf-8'/>
+		//       <title>การกดยืนยันการสมัค</title>
+		//     </head>
+		//     <body>
+		//       <div style='background: #214163;padding: 10px 0 20px 10px;margin-bottom:10px;font-size:30px;color:white;' >
+		//         <img src='https://drive.google.com/open?id=1d2dbqiwRVm4W7JfLpdHx17PKVL4aY1PR' style='width: 120px;'>
+		//         <div style='text-align:center'> 
+		//            <p>กดที่ลิงค์ เพื่อเปลี่ยนพาสเวิร์ด</p><br>
+		//            <p>URL : http://localhost:81/jobgis/checkmail.php?email=$email </p>
+		//         </div>
+		//       </div>
+		//         <div>       
 		          
-		        </div>
-		        <div style='margin-top:30px;'>
-		          <hr>
-		          <address>
-		            <h4>ติดต่อสอบถาม</h4>
-		            <p>กองถ่ายทอดเทคโนโลยีและนวัตกรรม
-		            มหาวิทยาลัยนเรศวร
-		            ชั้น 4 ตึก A อาคารมหาธรรมราชา ตำบลท่าโพธิ์ 
-		            อำเภอเมือง จังหวัดพิษณุโลก 65000
-		            </p>
-		            <p>www.facebook.com/Gistlnnu/</p>
-		          </address>
-		        </div>
-		      </div>
-		      <div style='background: #214163;color: #a2abb7;padding:30px;'>
-		        <div style='text-align:center'> 
-		           © กองถ่ายการทอดเทคโนโลยี มหาวิทยาลัยนเรศวร
-		        </div>
-		      </div>
-		    </body>
-		  </html>
-		";
+		//         </div>
+		//         <div style='margin-top:30px;'>
+		//           <hr>
+		//           <address>
+		//             <h4>ติดต่อสอบถาม</h4>
+		//             <p>กองถ่ายทอดเทคโนโลยีและนวัตกรรม
+		//             มหาวิทยาลัยนเรศวร
+		//             ชั้น 4 ตึก A อาคารมหาธรรมราชา ตำบลท่าโพธิ์ 
+		//             อำเภอเมือง จังหวัดพิษณุโลก 65000
+		//             </p>
+		//             <p>www.facebook.com/Gistlnnu/</p>
+		//           </address>
+		//         </div>
+		//       </div>
+		//       <div style='background: #214163;color: #a2abb7;padding:30px;'>
+		//         <div style='text-align:center'> 
+		//            © กองถ่ายการทอดเทคโนโลยี มหาวิทยาลัยนเรศวร
+		//         </div>
+		//       </div>
+		//     </body>
+		//   </html>
+		// ";
 
-		//  ถ้ามี email ผู้รับ
-		if($email_receiver){
-			$mail->msgHTML($email_content);
-
-
-			if (!$mail->send()) {  // สั่งให้ส่ง email
-
-				// กรณีส่ง email ไม่สำเร็จ
-				echo "<h3 class='text-center'>ระบบมีปัญหา กรุณาลองใหม่อีกครั้ง</h3>";
-				//echo $mail->ErrorInfo; // ข้อความ รายละเอียดการ error
-			}else{
-				// กรณีส่ง email สำเร็จ
-				echo "ระบบได้ส่งข้อความไปเรียบร้อย";
-			}	
-		}
-
-	  $result = pg_query($db,$sql2);
-
-		  if($result){ 
+		// //  ถ้ามี email ผู้รับ
+		// if($email_receiver){
+		// 	$mail->msgHTML($email_content);
 
 
-		  $message = '<div class="alert alert-success alert-dismissible">
-								<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-								<strong>Success!</strong> ลงทะเบียนเสร็จสิ้นแล้ว
-							  </div>';
-			//header('Location:checklogin.php?user_email='.$email.'&user_password='.$telephone.'&login=Login');
-			header('Location: checkmail.php?email=$email&type=submit_mail');
-				  exit;
+		// 	if (!$mail->send()) {  // สั่งให้ส่ง email
 
-		  }else{
+		// 		// กรณีส่ง email ไม่สำเร็จ
+		// 		echo "<h3 class='text-center'>ระบบมีปัญหา กรุณาลองใหม่อีกครั้ง</h3>";
+		// 		//echo $mail->ErrorInfo; // ข้อความ รายละเอียดการ error
+		// 	}else{
+		// 		// กรณีส่ง email สำเร็จ
+		// 		echo "ระบบได้ส่งข้อความไปเรียบร้อย";
+		// 	}	
+		// }
 
-			  $message = '<div class="alert alert-danger alert-dismissible">
-						  <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-						  <strong>Warning!</strong> ไม่สามา่รถบันทึกข้อมูลได้ กรุณาลองอีกครั้ง
-						 </div>';
+	 //  $result = pg_query($db,$sql2);
 
-		  }
+		//   if($result){ 
+
+
+		//   $message = '<div class="alert alert-success alert-dismissible">
+		// 						<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+		// 						<strong>Success!</strong> ลงทะเบียนเสร็จสิ้นแล้ว
+		// 					  </div>';
+		// 	//header('Location:checklogin.php?user_email='.$email.'&user_password='.$telephone.'&login=Login');
+		// 	header('Location: checkmail.php?email=$email&type=submit_mail');
+		// 		  exit;
+
+		//   }else{
+
+		// 	  $message = '<div class="alert alert-danger alert-dismissible">
+		// 				  <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+		// 				  <strong>Warning!</strong> ไม่สามา่รถบันทึกข้อมูลได้ กรุณาลองอีกครั้ง
+		// 				 </div>';
+
+		//   }
 
 	  }else{ 
 			 $message = '<div class="alert alert-danger alert-dismissible">
@@ -246,7 +246,7 @@
 			<div class="container">
 				<div class="box-wrapper">				
 					<div class="box box-border">
-					<?php echo $message; ?>
+					<?php echo $sql2; ?>
 						<div class="box-body">
 							<h4>ลงทะเบียนเข้าใช้งาน</h4>
 							<small>* กรุณากรอกข้อมูลให้ครบถ้วน</small>
